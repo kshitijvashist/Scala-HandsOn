cff8c60094bc158602a9fe220c3267ed58c594e1
Kshitij Vashist <23kshitij92@gmail.com>
2022-04-23 09:32:27 +0200

    Basic Arithmetic Operations

---
+++ src/main/scala/basic_operations/ArithmeticOperation.scala
@@ -0,0 +1,14 @@
+package basic_operations
+
+object ArithmeticOperation {
+  def main(args: Array[String]): Unit = {
+    val a = 10;
+    var b = 10;
+    b=8;
+    println(a + b);
+    println(a -b);
+    println("Multiplication="+a * b);
+    println("Division="+a / b);
+  }
+
+}

2a02a4aa1fc32fc169334ffd90c8624e04b2f1f0
Kshitij Vashist <23kshitij92@gmail.com>
2022-04-23 10:53:38 +0200

    Basic Assignment Operations

---
+++ src/main/scala/basic_operations/AssignmentOperation.scala
@@ -0,0 +1,15 @@
+package basic_operations
+
+object AssignmentOperation {
+  def main(args: Array[String]): Unit = {
+    val a=10;
+    println(10==0);
+    println(a);
+    println(a>>10);
+    println(a<<10);
+    println(a>=10);
+    println(a<=0);
+    println(a!=10);
+
+  }
+}

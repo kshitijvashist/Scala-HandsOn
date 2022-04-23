0df13e29945ccd8b896e2d6646119411cf1aa169
Kshitij Vashist <23kshitij92@gmail.com>
2022-04-23 19:34:28 +0200

    Basic Closure Function in scala

--- src/main/scala/functions_basics/ClosureFunction.scala
+++ src/main/scala/functions_basics/ClosureFunction.scala
@@ -3,8 +3,11 @@ package functions_basics
 object ClosureFunction {
   def main(args: Array[String]): Unit = {
     println(callToClosureFunction(10,30));
+    println(callToClosureFunctionWithString("Sachin ","Tendulkar"));
+    callToClosureFunctionWithBothStrings("Kapil ","Dev");
   }
   val callToClosureFunction=(a:Int,b:Int)=>a+b;
-
+  val callToClosureFunctionWithString=(a:String,b:String)=>a+b;
+  val callToClosureFunctionWithBothStrings=(a:String,b:String)=>println(s"Best Cricket Player is $a $b");
 
 }

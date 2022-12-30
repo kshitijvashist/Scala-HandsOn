a2e77dc4fc1e86ace2334dd0e3a13f81fd2835fb
Kshitij Vashist <23kshitij92@gmail.com>
2022-12-30 19:09:22 +0100

    CallByName and CallByValue Differences using Scala

--- src/main/scala/functions_basics/CalledByName.scala
+++ src/main/scala/functions_basics/CalledByName.scala
@@ -1,17 +1,19 @@
 package functions_basics
 
+import java.util.UUID
+
 object CalledByName extends App {
 
-  def printGivenTimeCallByValue(time: Long): Unit = {
-    println(s"Print current time: $time")
-    println(s"Print current time Again: $time")
+  def printGivenTimeCallByValue(time: UUID): Unit = {
+    println(s"Print Random UUID: $time")
+    println(s"Print Random UUID Again: $time")
   }
 
-  def printGivenTimeCallByName(time: => Long): Unit = {
-    println(s"Print current time: $time")
-    println(s"Print current time Again: $time")
+  def printGivenTimeCallByName(time: => UUID): Unit = {
+    println(s"Print Random UUID: $time")
+    println(s"Print Random UUID Again: $time")
   }
 
-  printGivenTimeCallByValue(System.nanoTime())
-  printGivenTimeCallByName(System.nanoTime())
+  printGivenTimeCallByValue(UUID.randomUUID())
+  printGivenTimeCallByName(UUID.randomUUID())
 }
